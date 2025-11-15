3cf120871ada37d293b1f22abf3d8773a2d02935
Jhon Quintero <jfqm7710@gmail.com>
2025-11-15 12:13:37 -0500

    feat(notification-push): handling push notifications from box

--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -82,7 +82,7 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
     debugPrint('FCM Token: $token');
   }
 
-  void _handleRemoteMessage(RemoteMessage message) {
+  void handleRemoteMessage(RemoteMessage message) {
     if (message.notification == null) return;
 
     final notification = PushMessage(
@@ -101,7 +101,7 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   }
 
   void _onForegroundMessage() {
-    FirebaseMessaging.onMessage.listen(_handleRemoteMessage);
+    FirebaseMessaging.onMessage.listen(handleRemoteMessage);
   }
 
   PushMessage? getPushMessage(String messageId) {
